01bcf920e638427ae5b6c95227a3fb24de209e54
Collin <Collin.christian70@tamu.edu>
2021-09-23 22:33:11 -0500

    Python Script now opens the Log_file_download.txt
    with this addition we can open the log file and it prints the entire contents in the terminal.

--- Python.py
+++ Python.py
@@ -2,3 +2,7 @@ from urllib import request
 remote_url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 local_file = 'Log_file_download.txt'
 request.urlretrieve(remote_url, local_file)
+
+with open('Log_file_download.txt') as f:
+    contents = f.read()
+    print(contents)
