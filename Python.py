6b0cba2dee334aaf975ecb9805b164df52c5c960
AntonioSanchez99 <antoniosanchez99@tamu.edu>
2021-09-26 15:41:24 -0500

    Added cod to count the requests made in the whole file

--- Python.py
+++ Python.py
@@ -1,4 +1,9 @@
+from typing import Counter
 from urllib import request
 remote_url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 local_file = 'Log_file_download.txt'
 request.urlretrieve(remote_url, local_file)
+
+count = 0
+for line in open('Log_file_download.txt'): count += 1 
+print ('There were ', count, ' requests made in teh time period rpresented by the log')
