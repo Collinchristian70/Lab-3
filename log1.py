c24fb96c9fe476115fc31728b89fe46799d6ab62
konnorscott22 <konnorscott@tamu.edu>
2021-10-11 16:06:19 -0500

    Delete loganalyze

--- log1.py
+++ log1.py
@@ -1,9 +1,9 @@
 import re
 #open https log file
-f = open('file.log','r')
+file = open('file.log','r')
 
-lines = f.readlines()
+lines = file.readlines()
 count=0 #set count to 0
 for line in lines:
     count=count+1
-print(count)
+    print(count)
