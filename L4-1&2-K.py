ed42413a5e21c97590b5969ff1de2eef3d5c6a02
konnorscott22 <konnorscott@tamu.edu>
2021-10-12 20:03:34 -0500

    Update L4-1&2-K.py

--- L4-1&2-K.py
+++ L4-1&2-K.py
@@ -2,9 +2,8 @@ import re
 import datetime
 
 # open the text file and read the data
-f = open('file.log','r')
-text = f.read()
-date = re.findall(r'(\d\d+/\w\w\w/\w\w\w+)',text)
+file = open('file.log','r')
+text = file.read()
 dt = datetime.strptime(logdate,%d/%b/%Y)
 
 
