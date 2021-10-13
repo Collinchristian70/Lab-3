f310b965245c8ab52320320fbdf250a03e0a9f94
konnorscott22 <konnorscott@tamu.edu>
2021-10-12 19:47:40 -0500

    Update L4-1&2-K.py

--- L4-1&2-K.py
+++ L4-1&2-K.py
@@ -2,8 +2,8 @@ import re
 import datetime
 
 # open the text file and read the data
-file = open('file.log','r')
-text = file.read()
+f = open('file.log','r')
+text = f.read()
 date = re.findall(r'(\d\d+/\w\w\w/\w\w\w+)',text)
 dt = datetime.strptime(logdate,%d/%b/%Y)
 
@@ -11,5 +11,3 @@ dt = datetime.strptime(logdate,%d/%b/%Y)
 matches = re.findall(r'(\d\d+/\w\w\w/\d\d\d\d+)'
 df= datetime.strptime(log_date, "%d/%b/%Y")
 print(df)
-
-    
